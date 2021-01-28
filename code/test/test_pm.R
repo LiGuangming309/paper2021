3457a73363c2e23dc41acc696d095abcce0b7c07
FridljDa <daniel.fridljand@gmail.com>
2021-01-28 21:34:47 +0100

    afw

---
+++ code/test/test_pm.R
@@ -0,0 +1,73 @@
+packages <- c("dplyr", "magrittr", "data.table", "testthat", "tidyverse", "tictoc", "viridis", "hrbrthemes")
+
+for (p in packages) {
+  suppressMessages(library(p, character.only = T, warn.conflicts = FALSE, quietly = TRUE))
+}
+options(dplyr.summarise.inform = FALSE)
+options(dplyr.join.inform = FALSE)
+
+# TODO delete
+#if (rlang::is_empty(args)) {
+  tmpDir <- "/Users/default/Desktop/paper2021/data/tmp"
+  censDir <- "/Users/default/Desktop/paper2021/data/05_demog"
+  dem_agrDir <- "/Users/default/Desktop/paper2021/data/06_dem.agr"
+#}
+
+dem_agrDir <- file.path(dem_agrDir, "nation")
+states <- file.path(tmpDir, "states.csv") %>% read.csv()
+## --------- read pm data -----------
+tic(paste("aggregated pm data"))
+dem_agr <- lapply(2000:2016, function(year) {
+  tic(year)
+    dem_agrDirY<-file.path(dem_agrDir,toString(year), paste0("cens_agr_", toString(year), "_us.csv"))
+    dem_agr <-fread(dem_agrDirY)
+  
+  meta <- read.csv(file.path(censDir, "meta", paste0("cens_meta_", year, ".csv")))
+  dem_agr<- dem_agr %>%
+    left_join(meta, by = "variable")
+  toc()
+  return(dem_agr)
+}) %>%
+  do.call(rbind, .) %>%
+  as.data.frame
+
+dem_agr <- dem_agr %>%
+  group_by(year,race, hispanic_origin,pm) %>%
+  summarise(pop_size = sum(pop_size))
+
+toc()
+## --------- read demographic census data -----------
+tic(paste("aggregated census data by"))
+censData_agr <- lapply(2000:2016, function(year) {
+  censData_agr <- apply(states, 1, function(state) {
+    STUSPS <- state["STUSPS"]
+    name <- state["NAME"]
+    censData_agrDir<-file.path(censDir, year, paste0("agr_census_", toString(year), "_", STUSPS, ".csv"))
+    censData_agr <-fread(censData_agrDir)
+    return(censData_agr)
+  }) %>%
+    do.call(rbind, .) %>%
+    as.data.frame
+  
+  meta <- read.csv(file.path(censDir, "meta", paste0("cens_meta_", year, ".csv")))
+  censData_agr<- censData_agr %>%
+    left_join(meta, by = "variable")
+
+  return(censData_agr)
+}) %>%
+  do.call(rbind, .) %>%
+  as.data.frame()
+
+censData_agr <- censData_agr %>%
+  group_by(year, race, hispanic_origin) %>%
+  summarise(pop_size = sum(pop_size))
+
+toc()
+## --- calculate -----
+join <- inner_join(dem_agr, censData_agr, by = c("year", "race", "hispanic_origin"))
+join$prop <- join$pop_size.x/join$pop_size.y
+join$mean <- join$prop * join$pm
+
+join <- join %>%
+  group_by(year, race, hispanic_origin) %>%
+  summarise(mean = sum(mean))
