35adb107b85ace9402a3ffa26737fc729754d1a8
FridljDa <daniel.fridljand@gmail.com>
2021-02-05 18:20:12 +0100

    aw

--- code/00_parent.R
+++ code/00_parent.R
@@ -187,6 +187,6 @@ args <- paste(
   data.dir #8
 ) 
 
-runscript(script = summary.script, args = args)
-runscript(script = plot.script, args = args)
+#runscript(script = summary.script, args = args)
+#runscript(script = plot.script, args = args)
   
