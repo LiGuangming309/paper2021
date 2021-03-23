b0c595e60c2f57512fec5e81bad8dd407849b24b
FridljDa <daniel.fridljand@gmail.com>
2021-03-23 11:22:52 +0100

    af

--- code/11_calc_attr_burd.R
+++ code/11_calc_attr_burd.R
@@ -31,8 +31,8 @@ attrBurdenDir <- args[13]
 
 # TODO delete
 if (rlang::is_empty(args)) {
-  year <- 2000
-  agr_by <- "STATEFP"
+  year <- 2001
+  agr_by <- "nation"
 
   dataDir <- "/Users/default/Desktop/paper2021/data"
   tmpDir <- "/Users/default/Desktop/paper2021/data/tmp"
@@ -58,13 +58,16 @@ if (!file.exists(attrBurdenDir)) {
   ## ----determine join variables
   join_variables <- c(
     "Year" = "year",
-    # "Year.code"="year",
-    #"Gender" = "gender",
-    #"Gender.Code" = "gender_label",
     "Race" = "race",
     "Hispanic.Origin" = "hispanic_origin",
     "label_cause" = "label_cause"
   )
+  
+  if(agr_by == "nation"){
+    join_variables <- c(join_variables,
+                        "Gender" = "gender",
+                        "Gender.Code" = "gender_label")
+  }
 
   agr_by_replace <- c(
     "county" = "County", "Census_Region" = "Census.Region.Code", "Census_division" = "Census.Division.Code",
@@ -144,13 +147,21 @@ if (!file.exists(attrBurdenDir)) {
   files <- list.files(totalBurdenDir)
   total_burden <- lapply(files, function(file) {
     fileDir <- file.path(totalBurdenDir, file)
+    total_burden <- read.delim(fileDir)
 
-    columns <- c(unname(inverse_join_variables), "Notes", "Single.Year.Ages", "Single.Year.Ages.Code", "Deaths")
-    if (agr_by_new == "Nation") columns <- columns[columns != "Nation"] # in this case this column does not exist
-
-    total_burden <- read.delim(fileDir) %>%
-      select(any_of(columns)) %>%
-      filter(Single.Year.Ages != "Not Stated")
+    if (agr_by_new == "Nation"){
+      columns <- c(unname(inverse_join_variables), "Notes", "Deaths", "Single.Year.Ages", "Single.Year.Ages.Code")
+      columns <- columns[columns != "Nation"] # in this case this column does not exist
+      
+      total_burden <- total_burden%>%
+        select(any_of(columns)) %>%
+        filter(Single.Year.Ages != "Not Stated")
+    }else{
+      columns <- c(unname(inverse_join_variables), "Notes", "Deaths", "Five.Year.Age.Groups", "Five.Year.Age.Groups.Code") 
+      total_burden <- total_burden%>%
+        select(any_of(columns)) %>%
+        filter(Five.Year.Age.Groups != "Not Stated")
+    } 
 
     cause_icd <- total_burden$Notes[grepl("UCD - ICD-10 Codes:", total_burden$Notes, fixed = TRUE)]
     notes_hisp_or <- total_burden$Notes[grepl("Hispanic Origin:", total_burden$Notes, fixed = TRUE)]
@@ -158,21 +169,25 @@ if (!file.exists(attrBurdenDir)) {
     total_burden <- total_burden %>% subset(select = -Notes)
     total_burden <- total_burden[!apply(is.na(total_burden) | total_burden == "", 1, all), ]
 
-    if (grepl("I20-I25 (Ischaemic heart diseases)", cause_icd, fixed = TRUE)) {
-      total_burden$label_cause <- "cvd_ihd"
-    } else if (grepl("J41.0 (Simple chronic bronchitis); J41.1 (Mucopurulent chronic bronchitis); J41.8 (Mixed simple and", cause_icd, fixed = TRUE)) {
-      total_burden$label_cause <- "resp_copd"
-    } else if (grepl("E11.0 (Non-insulin-dependent diabetes mellitus, with coma); E11.1 (Non-insulin-dependent diabetes mellitus", cause_icd, fixed = TRUE)) {
-      total_burden$label_cause <- "t2_dm"
-    } else if (grepl("G45.0 (Vertebro-basilar artery syndrome); G45.1 (Carotid artery syndrome (hemispheric)); G45.2 (Multiple and", cause_icd, fixed = TRUE)) {
-      total_burden$label_cause <- "cvd_stroke"
-    } else if (grepl("A48.1 (Legionnaires disease); A70 (Chlamydia psittaci infection); B34.2 (Coronavirus infection,", cause_icd, fixed = TRUE)) {
-      total_burden$label_cause <- "lri"
-    } #else if (grepl("C33 (Malignant neoplasm of trachea); C44.0 (Skin of lip - Malignant neoplasms); C44.1 (Skin of eyelid,", cause_icd, fixed = TRUE)) {
+    if(rlang::is_empty(cause_icd)){
+      total_burden$label_cause <- "all-cause"
+    }else{
+      if (grepl("I20-I25 (Ischaemic heart diseases)", cause_icd, fixed = TRUE)) {
+        total_burden$label_cause <- "cvd_ihd"
+      } else if (grepl("J41.0 (Simple chronic bronchitis); J41.1 (Mucopurulent chronic bronchitis); J41.8 (Mixed simple and", cause_icd, fixed = TRUE)) {
+        total_burden$label_cause <- "resp_copd"
+      } else if (grepl("E11.0 (Non-insulin-dependent diabetes mellitus, with coma); E11.1 (Non-insulin-dependent diabetes mellitus", cause_icd, fixed = TRUE)) {
+        total_burden$label_cause <- "t2_dm"
+      } else if (grepl("G45.0 (Vertebro-basilar artery syndrome); G45.1 (Carotid artery syndrome (hemispheric)); G45.2 (Multiple and", cause_icd, fixed = TRUE)) {
+        total_burden$label_cause <- "cvd_stroke"
+      } else if (grepl("A48.1 (Legionnaires disease); A70 (Chlamydia psittaci infection); B34.2 (Coronavirus infection,", cause_icd, fixed = TRUE)) {
+        total_burden$label_cause <- "lri"
+      } #else if (grepl("C33 (Malignant neoplasm of trachea); C44.0 (Skin of lip - Malignant neoplasms); C44.1 (Skin of eyelid,", cause_icd, fixed = TRUE)) {
       else if (grepl("C33 (Malignant neoplasm of trachea); C34.0 (Main bronchus - Malignant neoplasms); C34.1 (Upper lobe,", cause_icd, fixed = TRUE)) {
-      total_burden$label_cause <- "neo_lung"
-    } else {
-      print("unidentifiable ICD Code")
+        total_burden$label_cause <- "neo_lung"
+      } else {
+        print("unidentifiable ICD Code")
+      }
     }
 
     if (!"Hispanic.Origin" %in% colnames(total_burden)) {
@@ -199,13 +214,43 @@ if (!file.exists(attrBurdenDir)) {
       Hispanic.Origin != "Not Stated"
     ) 
 
+  #parse age groups
+  if("Single.Year.Ages.Code" %in% colnames(total_burden)){
+    total_burden <- total_burden %>%
+      mutate(
+        min_age = as.numeric(Single.Year.Ages.Code),
+        max_age = as.numeric(Single.Year.Ages.Code)#,
+        #Single.Year.Ages.Code = NULL
+      )
+  }else if("Five.Year.Age.Groups.Code" %in% colnames(total_burden)){
+    total_burden <- total_burden %>%
+      mutate(
+        min_age = Five.Year.Age.Groups.Code %>% sapply(function(a) {
+          # extract first number in String
+          str_extract(a, "[:digit:]+") %>%
+            as.numeric()
+        }),
+        max_age = Five.Year.Age.Groups.Code %>% sapply(function(a) {
+          # if includes "+", max_age = 150, since humans do not get older
+          if (grepl("\\+", a)) {
+            return(150)
+          }
+          # otherwise extracts last number in String
+          last_num <- str_extract_all(a, "[:digit:]+") %>%
+            unlist() %>%
+            tail(1) %>%
+            as.numeric()
+        })#,
+        #Five.Year.Age.Groups.Code = NULL
+      )
+  }
   # ---------check for missing stuff----------------
   # missing Genders
-  missing <- setdiff(c("Male", "Female"), total_burden$Gender)
-  if (length(missing) > 0) {
-    print("Genders in total burden data missing:")
-    print(missing)
-  }
+  #missing <- setdiff(c("Male", "Female"), total_burden$Gender)
+  #if (length(missing) > 0) {
+  #  print("Genders in total burden data missing:")
+  #  print(missing)
+  #}
 
   # missing hispanic origin
   missing <- setdiff(c("Not Hispanic or Latino", "Hispanic or Latino", "All Origins"), total_burden$Hispanic.Origin)
@@ -238,7 +283,7 @@ if (!file.exists(attrBurdenDir)) {
 
   # give some feedback on what is still missing
   # one side
-  missing_rows <- anti_join(total_burden, pafs, by = join_variables)
+  missing_rows <- anti_join(filter(total_burden, label_cause != "all-cause") , pafs, by = join_variables)
   if (nrow(missing_rows) > 0) {
     print(paste(nrow(missing_rows), "rows are still missing in pafs data for", agr_by, ":"))
     print(head(missing_rows))
@@ -252,31 +297,43 @@ if (!file.exists(attrBurdenDir)) {
     print(paste(nrow(missing_rows), "rows are still missing in total burden data for", agr_by, ":"))
     print(head(missing_rows))
   }
+  # analyse suppression
+  nrow_suppressed <- nrow(total_burden)
+  total_burden <- total_burden %>% filter(Deaths != "Suppressed")
+  print(paste(100*(1-nrow(total_burden)/nrow_suppressed), "% rows suppressed"))
+  total_burden <- total_burden %>% mutate(Deaths = as.numeric(Deaths))
   
-  # calculate YLL
-  total_burden <- total_burden %>%
+  ##--- calculate YLL-----
+  total_burden$measure <- "Deaths"
+  total_burden <- total_burden %>% rename(value = Deaths)
+  
+  total_burden_yll <- total_burden %>%
     mutate(
-      Single.Year.Ages.Code = as.numeric(Single.Year.Ages.Code),
-      Life.Expectancy = lifeExpectancy$Life.Expectancy[findInterval(Single.Year.Ages.Code, lifeExpectancy$Age)],
-      YLL = Deaths * Life.Expectancy
+      Life.Expectancy = lifeExpectancy$Life.Expectancy[findInterval(max_age, lifeExpectancy$Age)], #TODO max_age?
+      value = value * Life.Expectancy,
+      measure= "YLL",
+      Life.Expectancy = NULL 
     )
 
+  total_burden <- rbind(total_burden, total_burden_yll)
+  total_burden$attr <- "not attributable"
   ## ----- join total_burden and pafs-----
-
-  burden_paf <- inner_join(total_burden, pafs, by = join_variables)
+  
+  total_burden_cause <- total_burden %>% filter(label_cause != "all-cause")
+  burden_paf <- inner_join(total_burden_cause, pafs, by = join_variables)
 
   # filter those, where age in correct interval
   burden_paf <- burden_paf %>%
     filter(
-      (min_age <= Single.Year.Ages.Code & Single.Year.Ages.Code <= max_age) |
-        (Single.Year.Ages.Code == 100 & 100 <= min_age)
+      (min_age.x <= min_age.y & max_age.y <= max_age.x) |
+      (min_age.y <= min_age.x & max_age.x <= max_age.y)
     )
 
   ## ----- calculate attributable burden------
 
   test_that("09_calc distinct rows", {
     burden_paf_sub <- burden_paf %>%
-      select(Single.Year.Ages.Code, Gender.Code, Race, Hispanic.Origin, label_cause)
+      select(min_age.x, max_age.y, measure, Gender.Code, Race, Hispanic.Origin, label_cause)
 
     dub_ind <- duplicated(burden_paf_sub) | duplicated(burden_paf_sub, fromLast = TRUE)
     burden_paf_sub <- burden_paf[dub_ind, ]
@@ -286,39 +343,38 @@ if (!file.exists(attrBurdenDir)) {
 
   attrBurden <- burden_paf %>%
     mutate(
-      attrDeaths = Deaths * pafs,
-      attrYLL = YLL * pafs
+      value = value * pafs,
+      attr = "attributable"
     )
 
-  # group "out" Single.Age, since age group more appropriate
-  columns <- c(unname(inverse_join_variables), "min_age", "max_age")
+  # group "out" ages
+  columns <- c(unname(inverse_join_variables), "measure", "attr")
   attrBurden <- attrBurden %>%
     group_by_at(vars(one_of(columns))) %>%
-    summarize(
-      Deaths = sum(Deaths),
-      YLL = sum(YLL),
-      attrDeaths = sum(attrDeaths),
-      attrYLL = sum(attrYLL)
-    )
+    summarize(value = sum(value))
+  total_burden <- total_burden %>%
+    group_by_at(vars(one_of(columns))) %>%
+    summarize(value = sum(value))
 
+  attrBurden <- rbind(attrBurden, total_burden)
   # some basic tests
   test_that("09_read burden join2", {
     expect_false(any(is.na(attrBurden)))
 
     # test that total number of deaths/YLL has not changed
     columns <- unname(inverse_join_variables)
-    comp1 <- total_burden %>%
-      group_by_at(vars(one_of(columns))) %>%
-      summarize(Deaths = sum(Deaths), YLL = sum(YLL))
+    #comp1 <- total_burden %>%
+    #  group_by_at(vars(one_of(columns))) %>%
+    #  summarize(Deaths = sum(Deaths), YLL = sum(YLL))
 
-    comp2 <- attrBurden %>%
-      group_by_at(vars(one_of(columns))) %>%
-      summarize(Deaths = sum(Deaths), YLL = sum(YLL))
+    #comp2 <- attrBurden %>%
+    #  group_by_at(vars(one_of(columns))) %>%
+    #  summarize(Deaths = sum(Deaths), YLL = sum(YLL))
 
-    comp3 <- inner_join(comp1, comp2, by = columns)
+    #comp3 <- inner_join(comp1, comp2, by = columns)
 
-    expect_equal(comp3$Deaths.x, comp3$Deaths.x)
-    expect_equal(comp3$YLL.x, comp3$YLL.x)
+    #expect_equal(comp3$Deaths.x, comp3$Deaths.x)
+    #expect_equal(comp3$YLL.x, comp3$YLL.x)
   })
 
   fwrite(attrBurden, attrBurdenDir)
