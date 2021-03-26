7b892c190bf29d4a069b906cfe6264aed8092992
darfgun <daniel.fridljand@gmail.com>
2021-03-26 09:17:08 +0100

    afew

--- code/00_parent.R
+++ code/00_parent.R
@@ -126,7 +126,7 @@ plot.script <- file.path(code.dir, "13_plot.R")
 
 #--------parameters of code-------------------
 args <- paste(tmp.dir, exp.rr.dir)
-# runscript(script=mrbrtRR.script, args = args)
+ runscript(script=mrbrtRR.script, args = args)
 
  years <- c(2000,2010,2001:2009,2011:2016)
 #years <- c(2006)
@@ -159,7 +159,7 @@ for (year in years) {
   # runscript(script=assignTract.script, args = args)
   # runscript(script = assignTractAKHI.script, args = args)
   #  runscript(script = cens_agr.script, args = args)
-  # runscript(script = paf.script, args = args)
+   runscript(script = paf.script, args = args)
    runscript(script = calc.attr.burd.script, args = args)
 } 
 
@@ -175,5 +175,5 @@ args <- paste(
   data.dir # 8
 )
 
-# runscript(script = summary.script, args = args)
-# runscript(script = plot.script, args = args) 
+ runscript(script = summary.script, args = args)
+ runscript(script = plot.script, args = args) 
