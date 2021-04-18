84ba319bbfbbcdc682f8e40343373de79eb15cc3
darfgun <daniel.fridljand@gmail.com>
2021-04-18 16:03:51 +0200

    awf

--- code/00_parent.R
+++ code/00_parent.R
@@ -150,8 +150,8 @@ plot.script <- file.path(code.dir, "20_plot.R")
 args <- paste(tmp.dir, exp.rr.dir)
 # runscript(script=mrbrtRR.script, args = args)
 
-#years <- c(2000, 2010, 2001:2009, 2011:2016)
- years <- c(2009:2012)
+years <- c(2000, 2010, 2001:2009, 2011:2016)
+# years <- c(2009:2012)
 # years <- c(2001)
 for (agr_by in agr_bys) {
   for (source in sources) {
@@ -176,12 +176,12 @@ for (agr_by in agr_bys) {
         total.burden.parsed2.dir, #17
         attr.burden.dir # 18
       )
-       runscript(script = download.meta.script, args = args)
-       runscript(script = meta.cross.script, args = args)
+      # runscript(script = download.meta.script, args = args)
+      # runscript(script = meta.cross.script, args = args)
        if(year %in% c(2000, 2009:2016)){
       #   runscript(script = download.cens.script, args = args)
        } else{
-       #  runscript(script = interp.script, args = args)
+      #   runscript(script = interp.script, args = args)
        }
        
       # runscript(script = download.other.script, args = args)
@@ -195,8 +195,8 @@ for (agr_by in agr_bys) {
       #   runscript(script = read.nvs.findrepl.script, args = args)
       #  runscript(script = read.total.burden.nvs.script, args = args)
       }
-      runscript(script=pop.summary.script, args = args)
-     #   runscript(script=pop.summary.educ.script, args = args)
+      #runscript(script=pop.summary.script, args = args)
+        runscript(script=pop.summary.educ.script, args = args)
     #  runscript(script = add.rate.tot.burd, args = args)
     #  runscript(script = calc.attr.burd.script, args = args)
     }
