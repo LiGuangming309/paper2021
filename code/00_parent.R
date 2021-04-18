6e97407c786fb2dde5a9cda5087b5217d78c8c6c
FridljDa <daniel.fridljand@gmail.com>
2021-04-18 13:19:35 +0200

    awf

--- code/00_parent.R
+++ code/00_parent.R
@@ -150,8 +150,8 @@ plot.script <- file.path(code.dir, "20_plot.R")
 args <- paste(tmp.dir, exp.rr.dir)
 # runscript(script=mrbrtRR.script, args = args)
 
-years <- c(2000, 2010, 2001:2009, 2011:2016)
-# years <- c(2009:2011)
+#years <- c(2000, 2010, 2001:2009, 2011:2016)
+ years <- c(2009:2012)
 # years <- c(2001)
 for (agr_by in agr_bys) {
   for (source in sources) {
--- code/02_download_meta.R
+++ code/02_download_meta.R
@@ -31,7 +31,7 @@ censDir <- args[8]
 
 # TODO l?schen
 if (rlang::is_empty(args)) {
-  year <- 2009
+  year <- 2000
 
   # censDir <- "C:/Users/Daniel/Desktop/paper2020/data/06_demog"
   # tmpDir <-  "C:/Users/Daniel/Desktop/paper2020/data/tmp"
