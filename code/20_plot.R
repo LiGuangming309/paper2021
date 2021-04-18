164c77f1aea918b4f4d3ef5bffedaf325c3de596
FridljDa <daniel.fridljand@gmail.com>
2021-04-18 22:00:26 +0200

    aef

--- code/20_plot.R
+++ code/20_plot.R
@@ -45,43 +45,46 @@ plotDir <- file.path(plotDir, agr_by)
 dir.create(plotDir, recursive = T, showWarnings = F)
 
 attrBurden <- fread(file.path(summaryDir, "attr_burd.csv"))
-attrBurden <- attrBurden %>%
-  mutate(Ethnicity = paste0(Race, ", ", Hispanic.Origin)) %>%
-  filter(Ethnicity %in% c(
-    "White, Not Hispanic or Latino",
-    "White, Hispanic or Latino",
-    "Black or African American, All Origins",
-    "Asian or Pacific Islander, All Origins",
-    "American Indian or Alaska Native, All Origins"
-  )) #
-
 allBurden <- fread(file.path(summaryDir, "all_burd.csv"))
-allBurden <- allBurden %>%
-  mutate(Ethnicity = paste0(Race, ", ", Hispanic.Origin)) %>%
-  filter(Ethnicity %in% c(
-    "White, Not Hispanic or Latino",
-    "White, Hispanic or Latino",
-    "Black or African American, All Origins",
-    "Asian or Pacific Islander, All Origins",
-    "American Indian or Alaska Native, All Origins"
-  ))
+
+attrBurden <- attrBurden %>% filter(Gender.Code == "A")
+allBurden <- allBurden %>% filter(Gender.Code == "A")
+
+replaces3 <- data.frame(
+  Education = c(1:7, 666),
+  Education2 = c(
+    "Less than 9th grade", "9th to 12th grade, no diploma", "High school graduate, GED, or alternative",
+    "Some college, no degree", "Associate's degree", "Bachelor's degree", "Graduate or professional degree", "666"
+  )
+)
+attrBurden <- attrBurden %>% 
+  left_join(replaces3, by = "Education") %>% 
+  mutate(Ethnicity = paste0(Race, ", ", Hispanic.Origin)) 
+allBurden <- allBurden %>% 
+  left_join(replaces3, by = "Education") %>% 
+  mutate(Ethnicity = paste0(Race, ", ", Hispanic.Origin)) 
+rm(replaces3)
 
 agr_by_replace <- c(
   "county" = "County", "Census_Region" = "Census.Region.Code", "Census_division" = "Census.Division.Code",
   "hhs_region_number" = "HHS.Region.Code", "STATEFP" = "State.Code", "nation" = "nation", "county" = "County.Code"
 )
 agr_by_new <- agr_by_replace[[agr_by]]
-
+##-----by race -----
 for (location in attrBurden[, get(agr_by_new)] %>% unique()) {
   for (sourceX in attrBurden$source %>% unique()) {
     plotDirX <- file.path(plotDir, sourceX)
     dir.create(plotDirX, recursive = T, showWarnings = F)
 
     ### -------plot 1 ---------
-    allBurdenX <- allBurden %>% filter(
-      get(agr_by_new) == location,
-      source == sourceX
-    )
+    allBurdenX <- allBurden %>%
+      filter(Ethnicity %in% c(
+        "White, Not Hispanic or Latino",
+        "White, Hispanic or Latino",
+        "Black or African American, All Origins",
+        "Asian or Pacific Islander, All Origins",
+        "American Indian or Alaska Native, All Origins"
+      )) %>% filter(get(agr_by_new) == location, source == sourceX)
     allBurden1 <- allBurdenX %>% filter(measure1 == "YLL", measure2 == "crude rate")
 
     # https://www.datanovia.com/en/blog/how-to-create-a-ggplot-with-multiple-lines/
@@ -96,7 +99,7 @@ for (location in attrBurden[, get(agr_by_new)] %>% unique()) {
       guides(col = guide_legend(nrow = 3, byrow = TRUE)) +
       ggtitle(paste("crude rate YLL from all causes in", location))
 
-    ggsave(file.path(plotDirX, paste0(location, "_plot1.png")), plot = g)
+    ggsave(file.path(plotDirX, paste0(location, "_plot1_race.png")), plot = g)
     
     ### -------plot 2 ---------
     allBurden2 <- allBurdenX %>% filter(measure1 == "Deaths", measure2 == "age-adjusted rate")
@@ -111,14 +114,18 @@ for (location in attrBurden[, get(agr_by_new)] %>% unique()) {
       guides(col = guide_legend(nrow = 3, byrow = TRUE)) +
       ggtitle(paste("age-adjusted Deaths from all causes in", location))
     
-    ggsave(file.path(plotDirX, paste0(location, "_plot2.png")), plot = g)
+    ggsave(file.path(plotDirX, paste0(location, "_plot2_race.png")), plot = g)
 
     ### -------plot 3 ---------
-    attrBurdenX <- attrBurden %>% filter(
-      get(agr_by_new) == location,
-      source == sourceX
-    )
-    
+    attrBurdenX <- attrBurden %>%
+      filter(Ethnicity %in% c(
+        "White, Not Hispanic or Latino",
+        "White, Hispanic or Latino",
+        "Black or African American, All Origins",
+        "Asian or Pacific Islander, All Origins",
+        "American Indian or Alaska Native, All Origins"
+      )) %>% filter(get(agr_by_new) == location, source == sourceX)
+     
     attrBurden1 <- attrBurdenX %>%
       filter(measure1 == "YLL",
         measure2 == "crude rate",
@@ -135,10 +142,10 @@ for (location in attrBurden[, get(agr_by_new)] %>% unique()) {
       guides(col = guide_legend(nrow = 3, byrow = TRUE)) +
       ggtitle(paste("crude rate YLL directly attributable to PM exposure", location))
 
-    ggsave(file.path(plotDirX, paste0(location, "_plot3.png")), plot = g)
+    ggsave(file.path(plotDirX, paste0(location, "_plot3_race.png")), plot = g)
 
     g <- g + geom_ribbon(aes(ymin = lower, ymax = upper), linetype = 0, alpha = 0.1)
-    ggsave(file.path(plotDirX, paste0(location, "_plot3_conf.png")), plot = g)
+    ggsave(file.path(plotDirX, paste0(location, "_plot3_conf_race.png")), plot = g)
     
     ### -------plot 4 ---------
     attrBurden2 <- attrBurdenX %>%
@@ -154,10 +161,10 @@ for (location in attrBurden[, get(agr_by_new)] %>% unique()) {
       guides(col = guide_legend(nrow = 3, byrow = TRUE)) +
       ggtitle(paste("age-adjusted Deaths directly attributable to PM exposure", location))
     
-    ggsave(file.path(plotDirX, paste0(location, "_plot4.png")), plot = g)
+    ggsave(file.path(plotDirX, paste0(location, "_plot4_race.png")), plot = g)
     
     g <- g + geom_ribbon(aes(ymin = lower, ymax = upper), linetype = 0, alpha = 0.1)
-    ggsave(file.path(plotDirX, paste0(location, "_plot4_conf.png")), plot = g)
+    ggsave(file.path(plotDirX, paste0(location, "_plot4_conf_race.png")), plot = g)
 
     
     ### -------plot 5 -------
@@ -177,8 +184,118 @@ for (location in attrBurden[, get(agr_by_new)] %>% unique()) {
       guides(col = guide_legend(nrow = 3, byrow = TRUE)) +
       ggtitle("proportion of all YLL directly attributable to PM exposure")
 
-    ggsave(file.path(plotDirX, paste0(location, "_plot5.png")), plot = g)
+    ggsave(file.path(plotDirX, paste0(location, "_plot5_race.png")), plot = g)
     g <- g + geom_ribbon(aes(ymin = lower, ymax = upper), linetype = 0, alpha = 0.1)
-    ggsave(file.path(plotDirX, paste0(location, "_plot5_conf.png")), plot = g)
+    ggsave(file.path(plotDirX, paste0(location, "_plot5_conf_race.png")), plot = g)
   }
+}
+
+##-----by Education -----
+for (location in attrBurden[, get(agr_by_new)] %>% unique()) {
+    plotDirX <- file.path(plotDir, sourceX)
+    dir.create(plotDirX, recursive = T, showWarnings = F)
+    
+    allBurdenX <- allBurden %>%
+      filter(Education != 666)%>% 
+      filter(get(agr_by_new) == location,source == "wonder")
+    
+    ### -------plot 1 ---------
+    allBurden1 <- allBurdenX %>% filter(measure1 == "YLL", measure2 == "crude rate")
+    
+    # https://www.datanovia.com/en/blog/how-to-create-a-ggplot-with-multiple-lines/
+    # https://stackoverflow.com/questions/14794599/how-to-change-line-width-in-ggplot
+    g <- ggplot(allBurden1, aes(x = Year, y = overall_value)) +
+      geom_line(aes(color = Education2), size = 1) +
+      ylab(paste("YLL per 100.000")) +
+      xlab("Year") +
+      ylim(0, NA) +
+      xlim(2000, 2016) +
+      theme(legend.position = "bottom", legend.box = "vertical", legend.margin = margin()) +
+      guides(col = guide_legend(nrow = 3, byrow = TRUE)) +
+      ggtitle(paste("crude rate YLL from all causes in", location))
+    
+    ggsave(file.path(plotDirX, paste0(location, "_plot1_educ.png")), plot = g)
+    
+    ### -------plot 2 ---------
+    allBurden2 <- allBurdenX %>% filter(measure1 == "Deaths", measure2 == "age-adjusted rate")
+    
+    g <- ggplot(allBurden2, aes(x = Year, y = overall_value)) +
+      geom_line(aes(color = Education2), size = 1) +
+      ylab(paste("Deaths per 100.000")) +
+      xlab("Year") +
+      ylim(0, NA) +
+      xlim(2000, 2016) +
+      theme(legend.position = "bottom", legend.box = "vertical", legend.margin = margin()) +
+      guides(col = guide_legend(nrow = 3, byrow = TRUE)) +
+      ggtitle(paste("age-adjusted Deaths from all causes in", location))
+    
+    ggsave(file.path(plotDirX, paste0(location, "_plot2_educ.png")), plot = g)
+    
+    ### -------plot 3 ---------
+    attrBurdenX <- attrBurden %>%
+      filter(Education != 666)%>% 
+      filter(get(agr_by_new) == location,source == "wonder")
+    
+    attrBurden1 <- attrBurdenX %>%
+      filter(measure1 == "YLL",
+             measure2 == "crude rate",
+             attr == "attributable"
+      )
+    
+    g <- ggplot(attrBurden1, aes(x = Year, y = mean, color = Education2)) +
+      geom_line(size = 1) +
+      ylab(paste("YLL per 100.000")) +
+      xlab("Year") +
+      ylim(0, NA) +
+      xlim(2000, 2016) +
+      theme(legend.position = "bottom", legend.box = "vertical", legend.margin = margin()) +
+      guides(col = guide_legend(nrow = 3, byrow = TRUE)) +
+      ggtitle(paste("crude rate YLL directly attributable to PM exposure", location))
+    
+    ggsave(file.path(plotDirX, paste0(location, "_plot3_educ.png")), plot = g)
+    
+    g <- g + geom_ribbon(aes(ymin = lower, ymax = upper), linetype = 0, alpha = 0.1)
+    ggsave(file.path(plotDirX, paste0(location, "_plot3_conf_educ.png")), plot = g)
+    
+    ### -------plot 4 ---------
+    attrBurden2 <- attrBurdenX %>%
+      filter(measure1 == "Deaths", measure2 == "age-adjusted rate")
+    
+    g <- ggplot(attrBurden2, aes(x = Year, y = mean, color = Education2)) +
+      geom_line(size = 1) +
+      ylab(paste("Deaths per 100.000")) +
+      xlab("Year") +
+      ylim(0, NA) +
+      xlim(2000, 2016) +
+      theme(legend.position = "bottom", legend.box = "vertical", legend.margin = margin()) +
+      guides(col = guide_legend(nrow = 3, byrow = TRUE)) +
+      ggtitle(paste("age-adjusted Deaths directly attributable to PM exposure", location))
+    
+    ggsave(file.path(plotDirX, paste0(location, "_plot4_educ.png")), plot = g)
+    
+    g <- g + geom_ribbon(aes(ymin = lower, ymax = upper), linetype = 0, alpha = 0.1)
+    ggsave(file.path(plotDirX, paste0(location, "_plot4_conf_educ.png")), plot = g)
+    
+    
+    ### -------plot 5 -------
+    attrBurden3 <- attrBurdenX %>%
+      filter(
+        measure1 == "YLL",
+        measure2 == "prop. of overall burden",
+        attr == "attributable"
+      ) 
+    
+    g <- ggplot(attrBurden3, aes(x = Year, y = mean, color = Education2)) +
+      geom_line(size = 1) +
+      ylab(paste("%")) +
+      xlab("Year") +
+      xlim(2000, 2016) +
+      theme(legend.position = "bottom", legend.box = "vertical", legend.margin = margin()) +
+      guides(col = guide_legend(nrow = 3, byrow = TRUE)) +
+      ggtitle("proportion of all YLL directly attributable to PM exposure")
+    
+    ggsave(file.path(plotDirX, paste0(location, "_plot5_educ.png")), plot = g)
+    g <- g + geom_ribbon(aes(ymin = lower, ymax = upper), linetype = 0, alpha = 0.1)
+    ggsave(file.path(plotDirX, paste0(location, "_plot5_conf_educ.png")), plot = g)
+  
 }
