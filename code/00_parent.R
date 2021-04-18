64a07bb6e6f9fc658393d810fea2b01b5b6d536d
FridljDa <daniel.fridljand@gmail.com>
2021-04-18 12:57:24 +0200

    aef

--- code/00_parent.R
+++ code/00_parent.R
@@ -151,7 +151,7 @@ args <- paste(tmp.dir, exp.rr.dir)
 # runscript(script=mrbrtRR.script, args = args)
 
 #years <- c(2000, 2010, 2001:2009, 2011:2016)
- years <- c(2008:2011)
+ years <- c(2009:2011)
 # years <- c(2001)
 for (agr_by in agr_bys) {
   for (source in sources) {
@@ -195,8 +195,8 @@ for (agr_by in agr_bys) {
       #   runscript(script = read.nvs.findrepl.script, args = args)
       #  runscript(script = read.total.burden.nvs.script, args = args)
       }
-     # runscript(script=pop.summary.script, args = args)
-        runscript(script=pop.summary.educ.script, args = args)
+      runscript(script=pop.summary.script, args = args)
+     #   runscript(script=pop.summary.educ.script, args = args)
     #  runscript(script = add.rate.tot.burd, args = args)
     #  runscript(script = calc.attr.burd.script, args = args)
     }
