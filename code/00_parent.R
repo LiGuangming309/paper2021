8b6894c10de1767b22cf8042296ca841a70ad7b3
darfgun <daniel.fridljand@gmail.com>
2021-05-22 17:07:01 +0200

    af

--- code/00_parent.R
+++ code/00_parent.R
@@ -149,9 +149,9 @@ figure.script <- file.path(code.dir, "22_figure1.R")
 args <- paste(tmp.dir, exp.rr.dir)
 # runscript(script=mrbrtRR.script, args = args)
 
-#years <- c(2000, 2010, 2001:2009, 2011:2016)
+years <- c(2000, 2010, 2001:2009, 2011:2016)
 # years <- c(2000:2004)
- years <- c(2011)
+# years <- c(2011)
 for (agr_by in agr_bys) {
   for (source in sources) {
     for (year in years) {
