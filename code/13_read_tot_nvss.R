bd28b004cdb19dda6ce3ae7a19e547c425682f37
darfgun <daniel.fridljand@gmail.com>
2021-05-23 15:48:56 +0200

    aef

--- code/13_read_tot_nvss.R
+++ code/13_read_tot_nvss.R
@@ -37,6 +37,11 @@ if (rlang::is_empty(args)) {
   tmpDir <- "/Users/default/Desktop/paper2021/data/tmp"
   totalBurdenDir <- "/Users/default/Desktop/paper2021/data/08_total_burden"
   totalBurdenParsedDir <- "/Users/default/Desktop/paper2021/data/09_total_burden_parsed"
+
+  dataDir <- "C:/Users/Daniel/Desktop/paper2021/data"
+  tmpDir <- "C:/Users/Daniel/Desktop/paper2021/data/tmp"
+  totalBurdenDir <- "C:/Users/Daniel/Desktop/paper2021/data/08_total_burden"
+  totalBurdenParsedDir <- "C:/Users/Daniel/Desktop/paper2021/data/09_total_burden_parsed"
 }
 findreplace <- read.csv(file.path(totalBurdenParsedDir, "findreplace.csv")) %>% filter(Year == year)
 causes <- read.csv(file.path(totalBurdenParsedDir, "causes.csv")) 
@@ -308,20 +313,30 @@ if (!file.exists(totalBurdenParsedDir)) {
   total_burden_race <- total_burden %>% filter(Education == 666)
   total_burden_educ <- total_burden %>% filter(Education != 666 & max_age <= 64)
   
-  nrow_education <- nrow(total_burden_educ)
-  prop_education_unknown <- nrow(total_burden_educ %>% filter(Education == 9))/nrow_education
-  print(paste(100*prop_education_unknown, "% of all education unknown"))
-  prop_education_1989 <- nrow(total_burden_educ %>% filter(Education == 10))/nrow_education
-  print(paste(100*prop_education_1989, "% of all education has 1989 education revision and thus not comparible"))
+  prop_education_unknown <- total_burden_educ %>%
+    group_by_at(setdiff(inverse_selectcolumns, c("Education", "Deaths"))) %>%
+    mutate(propDeaths = Deaths/sum(Deaths), Deaths = NULL) %>%
+    filter(Education %in% 9:10) %>%
+    group_by_at(setdiff(inverse_selectcolumns, c("Education","propDeaths"))) %>%
+    summarise(propDeaths = sum(propDeaths))
+  
+  setdiff(inverse_selectcolumns, "Education")
+  colnames(total_burden_educ)
+  colnames(prop_education_unknown)
+  
+  total_burden_educ <- total_burden_educ %>% filter(Education %in% c(1:7))
+  total_burden_educ <- full_join(total_burden_educ, prop_education_unknown,
+                                  by = setdiff(inverse_selectcolumns, "Education")) %>%
+    mutate(propDeaths = replace_na(propDeaths, 0),
+           Deaths = Deaths/(1-propDeaths),
+           propDeaths = NULL)
   
   #counter, above effect
-  total_burden_educ <- total_burden_educ %>% filter(Education %in% c(1:7))
- # prop <- 1-prop_education_unknown-prop_education_1989
-  prop <- 1-prop_education_1989
-  total_burden_educ$Deaths <- total_burden_educ$Deaths/prop
   
   total_burden <- rbind(total_burden_race, total_burden_educ)
   #---write csv---
+  test_that("no na end read tot nvss", expect_false(any(is.na(total_burden))))
+  
   total_burden <- total_burden %>% tibble::add_column(source = "nvss")
   fwrite(total_burden, totalBurdenParsedDir)
   toc()
