6c45584870efa21cd3722af2a5d753577422cf78
darfgun <daniel.fridljand@gmail.com>
2021-06-05 15:02:43 +0200

    aef

--- code/00_parent.R
+++ code/00_parent.R
@@ -21,7 +21,7 @@ packages <- c(
 for (p in packages) {
   if (p %in% rownames(installed.packages()) == FALSE) {
     install.packages(p)
-  }
+  } 
 }
 
 # download rhdf5
@@ -190,10 +190,10 @@ for (agr_by in agr_bys) {
       # runscript(script = assignTractAKHI.script, args = args)
       #  runscript(script = cens_agr.script, args = args)
       #  runscript(script = paf.script, args = args)
-    #     runscript(script = read.nvs.findrepl.script, args = args)
-    #    runscript(script = read.total.burden.nvs.script, args = args)
-    #  runscript(script=pop.summary.script, args = args)
-    #  runscript(script=pop.summary.educ.script, args = args)
+         runscript(script = read.nvs.findrepl.script, args = args)
+        runscript(script = read.total.burden.nvs.script, args = args)
+      runscript(script=pop.summary.script, args = args)
+      runscript(script=pop.summary.educ.script, args = args)
       runscript(script = add.rate.tot.burd, args = args)
       runscript(script = calc.attr.burd.script, args = args)
        runscript(script = calc.attr.burd.alt.script, args = args)
