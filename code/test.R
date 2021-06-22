1a1da9af263fcfb68369a70f7cb38b98e4d33ceb
darfgun <daniel.fridljand@gmail.com>
2021-06-22 18:32:39 +0200

    aef

--- code/test.R
+++
@@ -1,6 +0,0 @@
-test <- pop_summary %>%
-  filter(source2 == "Own Interpolation" & Education == 666 & agr_by == "nation" & Ethnicity != "All, All Origins")
-
-ggplot(test, aes(x = Year, y = Population, color = Ethnicity)) +geom_line()
-
-
