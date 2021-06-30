b4faaa8df2d545079d674edb7ed4353989165bb6
FridljDa <daniel.fridljand@gmail.com>
2021-06-30 10:51:03 +0200

    awf

--- code/00_parent.R
+++ code/00_parent.R
@@ -180,32 +180,32 @@ for (agr_by in agr_bys) {
         total.burden.parsed2.dir, #17   
         attr.burden.dir # 18
       )
-       runscript(script = download.meta.script, args = args)
-       runscript(script = meta.cross.script, args = args)
+  #     runscript(script = download.meta.script, args = args)
+  #     runscript(script = meta.cross.script, args = args)
        if(year %in% c(2000, 2009:2016)){
-       runscript(script = download.cens.script, args = args)
+    #   runscript(script = download.cens.script, args = args)
        } else if (year == 1990){
-         runscript(script = read1990.script, args = args)
+    #     runscript(script = read1990.script, args = args)
        }else{
-         runscript(script = interp.script, args = args)
+    #     runscript(script = interp.script, args = args)
        }
        if(year %in% c(1990,2000)){
-         runscript(script = cross.walk.script, args = args)
+    #     runscript(script = cross.walk.script, args = args)
        }
   
-            runscript(script = download.other.script, args = args)
+    #        runscript(script = download.other.script, args = args)
     
-     runscript(script=assignTract.script, args  = args)
-      runscript(script = assignTractAKHI.script, args = args)
-        runscript(script = cens_agr.script, args = args)
-        runscript(script = paf.script, args = args)
-         runscript(script = read.nvs.findrepl.script, args = args)
-        runscript(script = read.total.burden.nvs.script, args = args)
-      runscript(script=pop.summary.script, args = args)
-      runscript(script=pop.summary.educ.script, args = args)
-       runscript(script = add.rate.tot.burd, args = args)
-      runscript(script = calc.attr.burd.script, args = args)
-       runscript(script = calc.attr.burd.alt.script, args = args)  
+     #runscript(script=assignTract.script, args  = args)
+    #  runscript(script = assignTractAKHI.script, args = args)
+    #    runscript(script = cens_agr.script, args = args)
+    #    runscript(script = paf.script, args = args)
+    #     runscript(script = read.nvs.findrepl.script, args = args)
+    #    runscript(script = read.total.burden.nvs.script, args = args)
+    #  runscript(script=pop.summary.script, args = args)
+    #  runscript(script=pop.summary.educ.script, args = args)
+    #   runscript(script = add.rate.tot.burd, args = args)
+    #  runscript(script = calc.attr.burd.script, args = args)
+    #   runscript(script = calc.attr.burd.alt.script, args = args)  
     }  
   }
 }
@@ -228,10 +228,10 @@ for (agr_by in agr_bys) {
     
 # runscript(script = summary.script, args = args)
 # runscript(script = summary.other.script, args = args)
-#runscript(script = figure1.script, args = args)
-#runscript(script = figure2.script, args = args) 
-#runscript(script = figure3.script, args = args)
-#runscript(script = figure4.script, args = args)
+runscript(script = figure1.script, args = args)
+runscript(script = figure2.script, args = args) 
+runscript(script = figure3.script, args = args)
+runscript(script = figure4.script, args = args)
 
   
 
