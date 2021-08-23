876daa71cbdff7cfa1cc8bfc22e43e5d33d093bb
Daniel Fridljand <daniel.fridljand@gmail.com>
2021-08-23 11:57:37 +0200

    af

--- code/20_summary.R
+++ code/20_summary.R
@@ -34,10 +34,10 @@ if (rlang::is_empty(args)) {
   attrBurdenDir <- "/Users/default/Desktop/paper2021/data/13_attr_burd"
   summaryDir <- "/Users/default/Desktop/paper2021/data/14_summary"
   
-  tmpDir <- "C:/Users/Daniel/Desktop/paper2021/data/tmp"
-  totalBurdenParsed2Dir <-"C:/Users/Daniel/Desktop/paper2021/data/12_total_burden_parsed2"
-  attrBurdenDir <- "C:/Users/Daniel/Desktop/paper2021/data/13_attr_burd"
-  summaryDir <- "C:/Users/Daniel/Desktop/paper2021/data/14_summary"
+  #tmpDir <- "C:/Users/Daniel/Desktop/paper2021/data/tmp"
+  #totalBurdenParsed2Dir <-"C:/Users/Daniel/Desktop/paper2021/data/12_total_burden_parsed2"
+  #attrBurdenDir <- "C:/Users/Daniel/Desktop/paper2021/data/13_attr_burd"
+  #summaryDir <- "C:/Users/Daniel/Desktop/paper2021/data/14_summary"
 }
 
 states <- file.path(tmpDir, "states.csv") %>%
@@ -114,6 +114,8 @@ test_that(" basic checks", {
   
   test <- attrBurden[rowSums(is.na(attrBurden)) > 0, ]
   expect_false(any(is.na(attrBurden)))
+  new_DF <- attrBurden_prop[rowSums(is.na(attrBurden_prop)) > 0,]
+  
   expect_false(any(is.na(attrBurden_prop))) #TODO
   expect_false(any(is.na(all_burden)))
   # TODO
