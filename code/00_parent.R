f8463f745739b3e552b0a5fe080491221a488a6d
Daniel Fridljand <daniel.fridljand@gmail.com>
2021-08-24 16:35:26 +0200

    aw

--- code/00_parent.R
+++ code/00_parent.R
@@ -186,7 +186,7 @@ for (agr_by in agr_bys) {
         attr.burden.dir # 18
       )
     #   runscript(script = download.meta.script, args = args)
-    #   runscript(script = meta.cross.script, args = args)
+       runscript(script = meta.cross.script, args = args)
        if(year %in% c(2000, 2009:2016)){
   #     runscript(script = download.cens.script, args = args)
        } else if (year == 1990){
@@ -205,10 +205,10 @@ for (agr_by in agr_bys) {
     #  runscript(script = assignTractAKHI.script, args = args)
      #    runscript(script = cens_agr.script, args = args)
      #   runscript(script = paf.script, args = args)
-    #     runscript(script = read.nvs.findrepl.script, args = args)
-   #     runscript(script = read.total.burden.nvs.script, args = args)
+   #      runscript(script = read.nvs.findrepl.script, args = args)
+  #      runscript(script = read.total.burden.nvs.script, args = args)
    #   runscript(script=pop.summary.script, args = args)
-    runscript(script=pop.summary.educ.script, args = args)
+   # runscript(script=pop.summary.educ.script, args = args)
          
     #  runscript(script = add.rate.tot.burd, args = args)
     #  runscript(script = calc.attr.burd.script, args = args)
--- rural_urban_class.R
+++ rural_urban_class.R
@@ -31,66 +31,131 @@ pop.summary.dir <- args[16]
 # TODO delete
 if (rlang::is_empty(args)) {
   dataDir <- "/Users/default/Desktop/paper2021/data"
+  censDir <- "/Users/default/Desktop/paper2021/data/05_demog"
   tmpDir <- "/Users/default/Desktop/paper2021/data/tmp"
-  
-  #dataDir <- "C:/Users/Daniel/Desktop/paper2021/data"
-  #tmpDir <-  "C:/Users/Daniel/Desktop/paper2021/data/tmp"
-  #censDir <- "C:/Users/Daniel/Desktop/paper2021/data/05_demog"
-  #pop.summary.dir <- "C:/Users/Daniel/Desktop/paper2021/data/11_population_summary"
-}
 
-###----
-crosswalk90 <- read.csv(file.path(dataDir, paste0("crosswalk_", 1990,"_2010.csv"))) %>% 
-  select(trtidFrom = trtid90, trtidTo = trtid10, weight) %>%
+  # dataDir <- "C:/Users/Daniel/Desktop/paper2021/data"
+  # tmpDir <-  "C:/Users/Daniel/Desktop/paper2021/data/tmp"
+  # censDir <- "C:/Users/Daniel/Desktop/paper2021/data/05_demog"
+  # censDir <- "C:/Users/Daniel/Desktop/paper2021/data/05_demog"
+  # pop.summary.dir <- "C:/Users/Daniel/Desktop/paper2021/data/11_population_summary"
+}
+suppressMessages(
+  rural_urban_class_or <- read_excel(file.path(dataDir, "NCHSURCodes2013.xlsx"), .name_repair = "universal") %>%
+    rename(rural_urban_class = ..2013.code) %>%
+    select(FIPS.code, rural_urban_class)
+)
+
+## ---
+years <- c(1990, 2000)
+pop.sum <- lapply(years, function(year) {
+  meta <- file.path(censDir, "meta", paste0("cens_meta_", year, ".csv")) %>%
+    fread() %>%
+    filter(Gender.Code == "A" & Race == "All" & Hispanic.Origin == "All Origins" &
+      Education == 666)
+
+  pop.sum <- lapply(list.files(file.path(censDir, year)), function(file) {
+    fread(file.path(censDir, year, file))
+  }) %>%
+    rbindlist() %>%
+    filter(variable %in% meta$variable)
+
+  pop.sum <- pop.sum %>%
+    mutate(FIPS.code = paste0(state, str_pad(county, 3, pad = "0"))
+    %>% as.integer()) %>%
+    group_by(FIPS.code) %>%
+    summarise(pop_size = sum(pop_size)) %>%
+    mutate(fromYear = year)
+}) %>% rbindlist()
+
+
+### ----
+crosswalk90 <- read.csv(file.path(dataDir, paste0("crosswalk_", 1990, "_2010.csv"))) %>%
+  select(trtidFrom = trtid90, trtidTo = trtid10) %>%
   mutate(fromYear = 1990)
-crosswalk00 <- read.csv(file.path(dataDir, paste0("crosswalk_", 2000,"_2010.csv"))) %>% 
-  select(trtidFrom = trtid00, trtidTo = trtid10, weight)%>%
+crosswalk00 <- read.csv(file.path(dataDir, paste0("crosswalk_", 2000, "_2010.csv"))) %>%
+  select(trtidFrom = trtid00, trtidTo = trtid10) %>%
   mutate(fromYear = 2000)
 
 crosswalk <- rbind(crosswalk00, crosswalk90)
 rm(crosswalk00, crosswalk90)
+nrow(crosswalk)
 crosswalk <- crosswalk %>%
-  mutate(countyFrom = trtidFrom %>%
-           str_pad(., 11, pad = "0") %>%
-           substr(., 0, 5) %>%
-           as.integer(),
-         countyTo = trtidTo %>%
-           str_pad(., 11, pad = "0") %>%
-           substr(., 0, 5) %>%
-           as.integer())
+  transmute(
+    countyFrom = trtidFrom %>%
+      str_pad(., 11, pad = "0") %>%
+      substr(., 0, 5) %>%
+      as.integer(),
+    countyTo = trtidTo %>%
+      str_pad(., 11, pad = "0") %>%
+      substr(., 0, 5) %>%
+      as.integer(),
+    fromYear
+  ) %>%
+  distinct()
 
 crosswalk <- crosswalk %>%
-  group_by(fromYear, countyFrom, countyTo) %>%
-  summarise(weight = sum(weight)) %>%
-  filter(weight == max(weight)) %>%
-  mutate(weight = NULL) #TODO other method, use population size
+  left_join(pop.sum, by = c("countyFrom" = "FIPS.code", "fromYear" = "fromYear")) %>%
+  group_by(fromYear, countyTo) %>%
+  filter(pop_size == max(pop_size)) %>%
+  mutate(pop_size = NULL)
 
-crosswalk <- rbind(crosswalk,
-                   data.frame(fromYear = 2010,
-                              countyFrom = crosswalk$countyTo %>% unique,
-                              countyTo = crosswalk$countyTo %>% unique))
-###----
-suppressMessages(
-  rural_urban_class <- read_excel(file.path(dataDir, "NCHSURCodes2013.xlsx"), .name_repair = "universal") %>%
-    rename(rural_urban_class= ..2013.code) %>%
-    select(FIPS.code, rural_urban_class)
+filler <- merge(
+  data.frame(countyFrom = setdiff(rural_urban_class_or$FIPS.code, crosswalk$countyTo),
+             countyTo = setdiff(rural_urban_class_or$FIPS.code, crosswalk$countyTo)),
+  data.frame(fromYear = c(1990, 2000, 2010))
 )
+
+crosswalk <- rbind(
+  crosswalk,
+  data.frame(
+    fromYear = 2010,
+    countyFrom = crosswalk$countyTo %>% unique(),
+    countyTo = crosswalk$countyTo %>% unique()
+  ),
+  filler
+)
+
+test_that("rural urban class", {
+  expect_false(any(is.na(crosswalk)))
+  test <- crosswalk %>%
+    group_by(countyTo) %>%
+    summarise(n = n(), Years = list(fromYear)) %>%
+    filter(n != 3)
+  expect_equal(nrow(test), 0)
+})
+### ----
+
+test_that("rural urban class", {
+  expect_false(any(is.na(rural_urban_class_or)))
+  test <- rural_urban_class_or %>%
+    group_by(FIPS.code) %>%
+    summarise(n = n()) %>%
+    filter(n != 1)
+  expect_equal(nrow(test), 0)
+  
+  anti_test <- rural_urban_class_or %>%
+    anti_join(crosswalk, by = c("FIPS.code" = "countyTo"))
+  expect_equal(nrow(anti_test), 0)
+})
+
+rural_urban_class <- rural_urban_class_or %>%
+  left_join(crosswalk, by = c("FIPS.code" = "countyTo")) %>% 
+  distinct
+
+test_that("rural urban class", {
+  expect_false(any(is.na(rural_urban_class)))
+  test <- rural_urban_class %>%
+    group_by(FIPS.code) %>%
+    summarise(n = n(), Years = list(fromYear)) %>%
+    filter(n != 3)
+  expect_equal(nrow(test), 0)
+})
+
 rural_urban_class <- rural_urban_class %>%
-  left_join(crosswalk, by = c("FIPS.code" = "countyTo"))
-
-#rural_urban_class$countyFrom <- apply(rural_urban_class, 1, function(row){
-#  replace_na(row[["countyFrom"]],row[["FIPS.code"]])
-#})
-#rural_urban_class <- rural_urban_class %>%
-#  mutate(countyFrom = replace_na(FIPS.code))
-
-#test <- rural_urban_class %>%
-#  group_by(FIPS.code) %>%
-#  summarise(n = n(), Years = list(fromYear))
-
-rural_urban_class <- rural_urban_class%>%
   mutate(FIPS.code = countyFrom, countyFrom = NULL)
 
 write.csv(rural_urban_class,
-          file = file.path(dataDir, "rural_urban_class.csv"),
-          row.names = F)
+  file = file.path(dataDir, "rural_urban_class.csv"),
+  row.names = F
+)
