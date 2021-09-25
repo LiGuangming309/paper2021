ca38f0eb6b14e4e98197c9bd739b2eda8bba29e4
Daniel Fridljand <daniel.fridljand@gmail.com>
2021-09-25 08:17:25 +0200

    aef

--- code/00_parent.R
+++ code/00_parent.R
@@ -161,8 +161,8 @@ figure4.script <- file.path(code.dir, "26_figure4.R")
 args <- paste(tmp.dir, exp.rr.dir)
 # runscript(script=mrbrtRR.script, args = args)
 
-# years <- c(1990, 2000, 2010, 1991:1999, 2001:2009, 2011:2016)
- years <- c(2000)
+ years <- c(1990, 2000, 2010, 1991:1999, 2001:2009, 2011:2016)
+# years <- c(2000)
 # years <- c(2000,1990,1991) #,1990,1991
 for (agr_by in agr_bys) {
   for (source in sources) {
